893bad1751bdbefec445e82da3458f620c9775d4
Chyld Medford <chyld.medford@gmail.com>
2015-04-24 14:44:21 -0700

    fin

--- client/models/task.js
+++ client/models/task.js
@@ -5,14 +5,23 @@ angular.module('checklist')
   var fbTasks;
   var afTasks;
 
-   function init(){
-     fbTasks = $rootScope.fbRoot.child('users/' + $rootScope.activeUser.uid + '/tasks');
-     afTasks = $firebaseArray(fbTasks);
-   }
+  function init(){
+    fbTasks = $rootScope.fbRoot.child('users/' + $rootScope.activeUser.uid + '/tasks');
+    afTasks = $firebaseArray(fbTasks);
+    return afTasks;
+  }
+
+  function save(task){
+    return afTasks.$save(task);
+  }
 
   function add(task){
     return afTasks.$add(task);
   }
 
-  return {add: add, init: init};
+  function destroy(task){
+    return afTasks.$remove(task);
+  }
+
+  return {add: add, init: init, destroy: destroy, save: save};
 }]);
--- client/views/tasks/tasks.js
+++ client/views/tasks/tasks.js
@@ -2,7 +2,31 @@
 
 angular.module('checklist')
 .controller('TasksCtrl', ['$scope', 'Task', '$window', function($scope, Task, $window){
-  Task.init();
+  $scope.afTasks = Task.init();
+
+  $scope.sort = function(sortString){
+    var modifier = ($scope.taskOrder === sortString) ? '-' : '';
+    $scope.taskOrder = modifier + sortString;
+  };
+
+  $scope.toggleComplete = function(task){
+    Task.save(task);
+  };
+
+  $scope.deleteTask = function(task){
+    Task.destroy(task);
+  };
+
+  $scope.editTask = function(task){
+    task.dueDate = new Date(task.dueDate);
+    $scope.task = task;
+  };
+
+  $scope.updateTask = function(task){
+    $scope.task = {};
+    task.dueDate = task.dueDate.getTime();
+    Task.save(task);
+  };
 
   $scope.addTask = function(task){
     var o = {
@@ -14,8 +38,8 @@ angular.module('checklist')
     };
 
     Task.add(o)
-    .then(function(data){
-      console.info('data', data);
+    .then(function(){
+      $scope.task = {};
     });
   };
 }]);
